2c5e9b7a1e9cee13131b99501f7f0bf3002a5539
takagimeow <dev@takagi.app>
2021-09-20 20:28:09 +0900

    feat: new component added

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
 export { SimpleCarousel } from './components/SimpleCarousel';
+export { SimpleHorizontalList } from './components/SimpleHorizontalList';
 export { Banner } from './components/Banner';
+export { Card } from './components/Card';
